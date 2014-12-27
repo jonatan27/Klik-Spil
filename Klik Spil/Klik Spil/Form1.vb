c8b60a2473f4d581ecc8d71db1933cddca4fff5d
SamuelKlit <samuel-er@live.dk>
2014-12-27 21:00:03 +0100

    Selve spillet

---
+++ Klik Spil/Klik Spil/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Dim clicks As Integer = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        clicks += 1
+    End Sub
+End Class
